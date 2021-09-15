81cc78c077d54e9ba1b5a1f2fda0a4a8b97b2960
Raphael Fleckenstein <raphael_167@icloud.com>
2021-09-15 21:55:19 +0200

    Added Commandbar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './index.css'
+import { init } from 'commandbar';
+init('3ac34534');
+
+const loggedInUserId = '12345'; // example
+window.CommandBar.boot(loggedInUserId);
 
 createApp(App).use(router).use(store).mount('#app')
