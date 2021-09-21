57ec86efc1cb68469c52567383410e05f6a616cf
Raphael <raphael@thelifecompany.com>
2021-09-21 22:19:01 +0200

    Added Favicon

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="sidebar">Test</div>
+</template>
+
+<script>
+
+export default {
+    name:"Sidebar"
+}
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,12 +2,18 @@ import { init } from 'commandbar';
 init('3ac34534');
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '/src/components/Home.vue'
+import Insights from '/src/components/Insights.vue'
 
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home,
+    },
+    {
+        path: '/insights',
+        name: 'Insights',
+        component: Insights,
     },
 ]
 
