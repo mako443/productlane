9ba8c1b432a7b87b06fc0df1fe8581161d8f39cf
Raphael <raphael@thelifecompany.com>
2021-10-01 16:16:55 +0200

    blub

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,6 @@ import { init } from 'commandbar';
 init('3ac34534');
 import { createRouter, createWebHistory } from 'vue-router'
 import Roadmap from '/src/components/Roadmap.vue'
-import Insights from '/src/components/Insights.vue'
 
 const routes = [
     {
@@ -10,11 +9,6 @@ const routes = [
         name: 'Roadmap',
         component: Roadmap,
     },
-    {
-        path: '/insights',
-        name: 'Insights',
-        component: Insights,
-    }
 ]
 
 const router = createRouter({
